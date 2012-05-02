d000410374a52cf638086bc0253c26df3afd8454
Carlos Silva <cefs1984@yahoo.com.br>
2012-05-02 20:39:43 -0300

    RAILS BASE CHANGE REPOSITORY NAME

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ server "173.246.40.107", :web, :app, :db, primary: true
 
 set :root_user, "root"
 set :user, "ruby"
-set :application, "cefystemsbase"
+set :application, "cefsystemsbase"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
