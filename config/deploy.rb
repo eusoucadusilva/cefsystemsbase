7c93d804f334f63523196b21a40d25fba67adf57
Carlos Silva <cefs1984@yahoo.com.br>
2012-05-02 20:51:54 -0300

    agora vai o base

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ server "173.246.40.107", :web, :app, :db, primary: true
 set :root_user, "root"
 set :user, "ruby"
 set :application, "cefsystemsbase"
-set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
