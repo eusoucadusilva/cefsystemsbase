da737db4ec689e57db367cc02bc7e179005fbbb8
Carlos Silva <cefs1984@yahoo.com.br>
2012-05-02 20:43:01 -0300

    RAILS BASE CHANGE REPOSITORY NAME 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:cefs/#{application}.git"
+set :repository, "git@github.com:cefs/cefsystemsbase.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
